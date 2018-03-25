907e66d2a8abf180c341845f78afb7b9a7c5e696
Andrew Salveson <andrewsalveson@gmail.com>
2018-03-24 23:58:01 -0400

    add php stuff

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 # have to run these as root
 sudo apt-get update
-sudo apt-get install apache2 mariadb-server git -y
+sudo apt-get install apache2 mariadb-server git vim libapache2-mod-php7* php7.0-cli php7.0-mysqli -y
 
