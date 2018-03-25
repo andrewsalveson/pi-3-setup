47b477bb8ea3f7231fe3d51e52e8c26641c45cbe
Andrew Salveson <andrewsalveson@gmail.com>
2018-03-24 21:23:31 -0400

    remve keygen stuff

--- setup.sh
+++ setup.sh
@@ -4,11 +4,3 @@
 sudo apt-get update
 sudo apt-get install apache2 mariadb-server git -y
 
-# run this as normal user
-ssh-keygen -t rsa -b 4096 -C "andrewsalveson@gmail.com"
-# then some user interaction will have to happen here
-
-# start ssh-agent in background
-eval $"$(ssh-agent -s)"
-# add RSA -- should have used the default path above
-ssh-add ~/.ssh/id_rsa
